d2f21448e0167b91c4c1a763b7dff35ce5a3a2fb
Navin Pathak <np4navin@gmail.com>
2016-06-23 15:35:47 -0500

    Started Game Display activity from New Game Button

--- app/src/main/java/io/scoober/ulti/ulti_mate/MainMenuActivityFragment.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/MainMenuActivityFragment.java
@@ -1,10 +1,12 @@
 package io.scoober.ulti.ulti_mate;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,21 @@ public class MainMenuActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        View mainMenuLayout = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        Button resume = (Button) mainMenuLayout.findViewById(R.id.resumeButton);
+        Button newGameButton = (Button) mainMenuLayout.findViewById(R.id.new_game_button);
+
+        //TODO Resume button
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),GameDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return mainMenuLayout;
     }
 }
