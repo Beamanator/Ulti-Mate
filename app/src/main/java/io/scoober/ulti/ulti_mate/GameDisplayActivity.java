2013f47c2c7fa01795484bbd0cd6f6bb8d2c7a22
Alex Beaman <dabeamanator@gmail.com>
2016-06-23 12:37:50 -0500

    Initial Commit

---
+++ app/src/main/java/io/scoober/ulti/ulti_mate/GameDisplayActivity.java
@@ -0,0 +1,13 @@
+package io.scoober.ulti.ulti_mate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_display);
+    }
+}
