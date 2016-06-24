44b70339ea167a6906a67f2f80616ca21e09bc40
Navin Pathak <np4navin@gmail.com>
2016-06-23 23:17:58 -0500

    Added intent to start GameSetupActivity

--- app/src/main/java/io/scoober/ulti/ulti_mate/GameSetupActivity.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/GameSetupActivity.java
@@ -1,5 +1,6 @@
 package io.scoober.ulti.ulti_mate;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
@@ -57,6 +59,15 @@ public class GameSetupActivity extends AppCompatActivity {
 
         //SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.smartTab);
         //viewPagerTab.setViewPager(mViewPager);
+
+        Button createGameDisplay= (Button) findViewById(R.id.createGameDisplay);
+        createGameDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),GameDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
