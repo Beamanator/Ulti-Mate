e535b734aaaf9e5856d1c90b7a90a7941c436b4a
Navin Pathak <np4navin@gmail.com>
2016-06-23 20:30:07 -0500

    Cleaned formatting and Added TimePicker to game setup

--- app/src/main/java/io/scoober/ulti/ulti_mate/GameDetailFragment.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/GameDetailFragment.java
@@ -1,13 +1,25 @@
 package io.scoober.ulti.ulti_mate;
 
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,7 +40,58 @@ public class GameDetailFragment extends Fragment {
 
         Log.d("GameDetailFragment","Test2");
 
+        final Button softCapButton = (Button) gameDetailView.findViewById(R.id.softCapInput);
+        final Button hardCapButton = (Button) gameDetailView.findViewById(R.id.hardCapInput);
+
+        softCapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar currentTime = Calendar.getInstance();
+                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = currentTime.get(Calendar.MINUTE);
+                TimePickerDialog softCapTimePicker = new TimePickerDialog(getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        softCapButton.setText(to12Hr(hourOfDay, minute));
+                    }
+                },hour,minute,false);
+                softCapTimePicker.show();
+            }
+        });
+
+        hardCapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar currentTime = Calendar.getInstance();
+                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = currentTime.get(Calendar.MINUTE);
+                TimePickerDialog hardCapTimePicker = new TimePickerDialog(getActivity(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                //TODO Validation on date and time (must be greater than current)
+                                hardCapButton.setText(to12Hr(hourOfDay, minute));
+                            }
+                        },hour,minute,false);
+                hardCapTimePicker.show();
+            }
+        });
+
         return gameDetailView;
+    }
+
+    private static String to12Hr(int hourOfDay, int minute) {
+        String timeString = hourOfDay + ":" + minute;
+        SimpleDateFormat sdf24 = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        try {
+            Date timeAsDate = sdf24.parse(timeString);
+            SimpleDateFormat sdf12 = new SimpleDateFormat("h:mm a", Locale.getDefault());
+            return sdf12.format(timeAsDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
