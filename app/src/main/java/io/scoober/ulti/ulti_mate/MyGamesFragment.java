166b12a113fe8823aaf86a1bdff4dc6d3720db83
Navin Pathak <np4navin@gmail.com>
2016-06-27 09:54:40 -0500

    Changed from RecycleView to ListView

--- app/src/main/java/io/scoober/ulti/ulti_mate/MyGamesFragment.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/MyGamesFragment.java
@@ -1,5 +1,6 @@
 package io.scoober.ulti.ulti_mate;
 
+import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,45 +16,27 @@ import java.util.List;
  * A fragment representing a list of Items.
  * <p>
  */
-public class MyGamesFragment extends Fragment {
+public class MyGamesFragment extends ListFragment {
 
-    private RecyclerView.Adapter gamesListAdapter;
     private List<Game> games;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public MyGamesFragment() {
-    }
+    private MyGamesListAdapter gamesListAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onActivityCreated(Bundle savedInstanceState) {
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_my_games_list, container, false);
+        super.onActivityCreated(savedInstanceState);
 
         GameDbAdapter gameDbAdapter = new GameDbAdapter(getActivity().getBaseContext());
         gameDbAdapter.open();
         games = gameDbAdapter.getRecentGames(20,0);
         gameDbAdapter.close();
 
-        gamesListAdapter = new MyGamesListAdapter(games);
+        gamesListAdapter = new MyGamesListAdapter(getActivity(),games);
 
+        setListAdapter(gamesListAdapter);
 
-        // Set the adapter
-        Context context = view.getContext();
-        RecyclerView recyclerView = (RecyclerView) view;
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        recyclerView.setAdapter(gamesListAdapter);
-
+        registerForContextMenu(getListView());
         //TODO Add feature to get more than 20 games
-        return view;
     }
 
 }
--- app/src/main/java/io/scoober/ulti/ulti_mate/MyGamesListAdapter.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/MyGamesListAdapter.java
@@ -1,9 +1,10 @@
 package io.scoober.ulti.ulti_mate;
 
-import android.support.v7.widget.RecyclerView;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -11,14 +12,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class MyGamesListAdapter extends RecyclerView.Adapter<MyGamesListAdapter.GameViewHolder> {
+public class MyGamesListAdapter extends ArrayAdapter<Game> {
 
-    private List<Game> games;
-
-    public class GameViewHolder extends RecyclerView.ViewHolder {
+    public class GameViewHolder {
         public TextView title,score,date,teams;
         public GameViewHolder(View itemView) {
-            super(itemView);
             title = (TextView) itemView.findViewById(R.id.listItemGameTitle);
             score = (TextView) itemView.findViewById(R.id.listItemScores);
             date = (TextView) itemView.findViewById(R.id.listItemGameDate);
@@ -26,32 +24,59 @@ public class MyGamesListAdapter extends RecyclerView.Adapter<MyGamesListAdapter.
         }
     }
 
-    public MyGamesListAdapter(List<Game> games) {
-        this.games = games;
+    public MyGamesListAdapter(Context context, List<Game> objects) {
+        super(context, 0, objects);
     }
 
     @Override
-    public GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_my_games_list_row, parent, false);
-        return new GameViewHolder(itemView);
-    }
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-    @Override
-    public void onBindViewHolder(GameViewHolder holder, int position) {
+        GameViewHolder holder;
+
+        // analogous to onCreateViewHolder() from RecycleView.Adapter
+        if (convertView == null) {
+            // Inflate view
+            convertView = LayoutInflater.from(getContext()).
+                    inflate(R.layout.fragment_my_games_list_row, parent, false);
+
+            holder = new GameViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder= (GameViewHolder) convertView.getTag();
+        }
+
+        // analogous to onBindViewHolder() from RecycleView.Adapter
+        Game game = getItem(position);
         //TODO think of internationalization
-        Game game = games.get(position);
         holder.title.setText(game.getGameName());
         holder.date.setText(getDateString(game.getDate()));
         holder.score.setText(game.getTeam1Score() + "-" + game.getTeam2Score());
         holder.teams.setText(game.getTeam1Name() + " vs " + game.getTeam2Name());
 
+        return convertView;
     }
 
-    @Override
-    public int getItemCount() {
-        return games.size();
-    }
+//    @Override
+//    public GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View itemView = LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.fragment_my_games_list_row, parent, false);
+//        return new GameViewHolder(itemView);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(GameViewHolder holder, int position) {
+//        Game game = games.get(position);
+//        holder.title.setText(game.getGameName());
+//        holder.date.setText(getDateString(game.getDate()));
+//        holder.score.setText(game.getTeam1Score() + "-" + game.getTeam2Score());
+//        holder.teams.setText(game.getTeam1Name() + " vs " + game.getTeam2Name());
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return games.size();
+//    }
 
     //TODO Move to utils
     public String getDateString(long milli) {
