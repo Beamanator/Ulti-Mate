fb2d446c7482896e4665e47d3f972216fc88fc18
Alex Beaman <dabeamanator@gmail.com>
2016-06-28 15:15:07 -0500

    Hooked up game display edit fragment for viewing and editing old games

--- app/src/main/java/io/scoober/ulti/ulti_mate/GameDisplayActivity.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/GameDisplayActivity.java
@@ -36,6 +36,7 @@ public class GameDisplayActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         // handle different cases for where Activity is called from
+        Log.d("GameDispAct","display: " + displayToLaunch.name());
         switch (displayToLaunch) {
             case NEW:
                 GameDisplayFragment gameFrag1 = new GameDisplayFragment();
