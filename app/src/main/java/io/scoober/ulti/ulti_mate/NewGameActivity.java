32fe4c74024a0038e46a9ea1f48e85c27c0c4e06
Navin Pathak <np4navin@gmail.com>
2016-07-07 16:22:27 -0500

    created framework for game templates

---
+++ app/src/main/java/io/scoober/ulti/ulti_mate/NewGameActivity.java
@@ -0,0 +1,20 @@
+package io.scoober.ulti.ulti_mate;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class NewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
