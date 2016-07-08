aad7d685afe08a8152302436eee77a7291e79d53
Navin Pathak <np4navin@gmail.com>
2016-07-08 00:54:54 -0500

    Added intent to NewGameActivity and icons.

--- app/src/main/java/io/scoober/ulti/ulti_mate/NewGameActivity.java
+++ app/src/main/java/io/scoober/ulti/ulti_mate/NewGameActivity.java
@@ -1,11 +1,18 @@
 package io.scoober.ulti.ulti_mate;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class NewGameActivity extends AppCompatActivity {
+
+    private Button noTemplateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,42 @@ public class NewGameActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        noTemplateButton = (Button) findViewById(R.id.button_no_template);
+        noTemplateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),GameSetupActivity.class);
+                intent.putExtra(MainMenuActivity.GAME_ID_EXTRA, 0);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_new_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(id) {
+            case R.id.action_create_new_template:
+                // TODO
+                Intent intent = new Intent(getBaseContext(),GameSetupActivity.class);
+                intent.putExtra(MainMenuActivity.GAME_ID_EXTRA, 0);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
